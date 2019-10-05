a6fa4d644ee9afcf8e4c2c48002b64dc562433b7
Nhan Dao <nhan.dao@outlook.com>
2019-10-05 17:32:00 +0800

    Create Model Classes:     + GameData     + Settings     + Structure

---
+++ app/src/main/java/curtin/edu/au/mad_assignment/model/GameData.java
@@ -0,0 +1,11 @@
+package curtin.edu.au.mad_assignment.model;
+
+public class GameData {
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/curtin/edu/au/mad_assignment/model/Structure.java
@@ -0,0 +1,12 @@
+package curtin.edu.au.mad_assignment.model;
+
+public abstract class Structure {
+
+    private int imageId;
+
+    public Structure(int imageId)
+    {
+        this.imageId = imageId;
+    }
+
+}
