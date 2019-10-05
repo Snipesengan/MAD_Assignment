e85b9d2943c453a34438091f9f51ad5647654c11
Nhan Dao <nhan.dao@outlook.com>
2019-10-05 17:45:34 +0800

    Added all android studio files, don't know if that's good practice

---
+++ app/src/main/java/curtin/edu/au/mad_assignment/controller/MainActivity.java
@@ -0,0 +1,16 @@
+package curtin.edu.au.mad_assignment.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import curtin.edu.au.mad_assignment.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
